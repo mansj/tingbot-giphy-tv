458ffbb84dbc3663e61dfe2812ee7d69020e4f9a
Måns Jonasson <mans@thejonassons.com>
2016-05-13 16:23:29 +0200

    Temp
    Signed-off-by: Måns Jonasson <mans@thejonassons.com>

--- giphy-tv.tingapp/main.py
+++ giphy-tv.tingapp/main.py
@@ -1,20 +1,29 @@
 import tingbot
 from tingbot import *
 from urlparse import urlparse
-import os
+import sys, os
 import json
-import urllib
+import urllib2
 import string
 import hashlib
-import random
 
+# Debug sys to stdout
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
-giphy_tv_url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats'
+# Set Debug on/off
+debug = 1
 
 state = {}
-channels = {'cats', 'dogs'}
+channels = ['cats', 'dogs']
 
-imageloaded = 99
+# Set current channel
+currentchannel = 0
+
+# Define imageloader counter
+imageloaded = 0
+
+def get_channel(i):
+    return 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + channels[i]
 
 def filename_for_url(url):
     m = hashlib.md5()
@@ -22,36 +31,41 @@ def filename_for_url(url):
     thishash = m.hexdigest()
     return '/tmp/giphy-' + thishash + '_' + os.path.basename(urlparse(url).path)
 
-@every(minutes=10)
-def refresh_feed():
-    global imageloaded
-    image_urls = []
+def loading_screen(cur): 
+    screen.fill(color='black')
+    screen.text('Loading image ' + str(cur))
+    screen.update()
+    return
+
+@button.press('left')
+def on_left():
+
+    # Show static GIF
+    image_url = 'static.gif'
+    state['image'] = Image.load(image_url)
     
-    for x in range(0, 3): 
-        response = urllib.urlopen(giphy_tv_url)
-        data = json.loads(response.read())
+    image = state['image']
     
-        for key, value in data.iteritems():
-            if key != 'data': 
-                continue
-            else:
-                image_urls.append(str(value['fixed_height_downsampled_url']))
-            
-    imageloaded = 0
-    for image_url in image_urls:
-        filename = filename_for_url(image_url)
+    width_sf = 320.0 / image.size[0]
+    height_sf = 240.0 / image.size[1]
+    
+    sf = max(width_sf, height_sf)
+    
+    screen.image(state['image'], scale=sf)
+    screen.update()    
+
+    # Change channel!
+    global giphy_tv_url, currentchannel
+    if currentchannel + 1 >= len(channels): 
+        currentchannel = 0
+    else:
+        currentchannel = currentchannel + 1
         
-        if not os.path.exists(filename):
-            urllib.urlretrieve(image_url, filename)
-
-        imageloaded = imageloaded + 1
-        
-        screen.fill(color='red')
-        screen.text('Loading image ' + str(random.randint(0,199)) + str(imageloaded))
-
-
-    state['image_urls'] = image_urls
-    state['index'] = 0
+    giphy_tv_url = get_channel(currentchannel)
+    
+    print giphy_tv_url
+    
+    refresh_feed(1)
 
 @every(seconds=5)
 def next_image():
@@ -66,15 +80,6 @@ def next_image():
     
     image_url = state['image_urls'][state['index']]
     state['image'] = Image.load(filename_for_url(image_url))
-
-def loop():
-    if 'image' not in state:
-        screen.fill(color='black')
-        screen.text('Loading...')
-        return
-    else:
-        screen.fill(color='black')
-        screen.text('Done!')
     
     image = state['image']
     
@@ -84,6 +89,75 @@ def loop():
     sf = max(width_sf, height_sf)
     
     screen.image(state['image'], scale=sf)
+    screen.update()    
+
+
+@every(minutes=10)
+def refresh_feed(user_refreshed = 0):
+    screen.update()
+    global imageloaded, state
+    
+    image_urls = []
+    
+    for x in range(1, 11): 
+        response = urllib2.urlopen(giphy_tv_url)
+        data = json.loads(response.read())
+    
+        for key, value in data.iteritems():
+            if key != 'data': 
+                continue
+            else:
+                image_urls.append(str(value['fixed_height_downsampled_url']))
+                if debug:
+                    print('image_urls_append: ' + str(x))
+            
+    # Reset imageloaded counter
+    imageloaded = 0
+    
+    # Load all images in the image_urls list to files
+    for image_url in image_urls:
+        if not user_refreshed:
+            loading_screen(imageloaded + 1)
+        
+        filename = filename_for_url(image_url)
+
+        # Get file from URL if it's not already downloaded
+        if not os.path.exists(filename):
+            gif = urllib2.urlopen(image_url)
+            with open(filename,'wb') as output:
+                  output.write(gif.read())
+
+            if debug:
+                print('urlretrieve: ' + str(image_url))
+
+
+        imageloaded = imageloaded + 1
+
+    # Update state list
+    state['image_urls'] = image_urls
+    state['index'] = 0
+    
+def loop():
+    if 'image' not in state:
+        screen.fill(color='black')
+        screen.text('Loading...')
+        return
+
+    image = state['image']
+
+    width_sf = 320.0 / image.size[0]
+    height_sf = 240.0 / image.size[1]
+    
+    sf = max(width_sf, height_sf)
+    
+    screen.image(state['image'], scale=sf)
+    screen.update()
 
 # run the app
+
+# Define first channel
+giphy_tv_url = get_channel(0)
+
+
+# Start loop
 tingbot.run(loop)
