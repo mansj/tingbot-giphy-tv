7b216588781ce13083d9b1e64f4d35321637e2b7
Måns Jonasson <mans@thejonassons.com>
2016-05-15 13:47:35 +0200

    Small changes, not done

--- giphy-tv.tingapp/main.py
+++ giphy-tv.tingapp/main.py
@@ -6,6 +6,7 @@ import json
 import urllib2
 import string
 import hashlib
+import threading
 
 # Debug sys to stdout
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
@@ -22,6 +23,9 @@ currentchannel = 0
 # Define imageloader counter
 imageloaded = 0
 
+# Define threading list
+threads = []
+
 def get_channel(i):
     return 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + channels[i]
 
@@ -37,8 +41,14 @@ def loading_screen(cur):
     screen.update()
     return
 
+def update_screen():
+    while 1 == 1:
+        screen.update()
+
 @button.press('left')
 def on_left():
+
+    global threads
 
     # Show static GIF
     image_url = 'static.gif'
@@ -52,7 +62,7 @@ def on_left():
     sf = max(width_sf, height_sf)
     
     screen.image(state['image'], scale=sf)
-    screen.update()    
+    screen.update()
 
     # Change channel!
     global giphy_tv_url, currentchannel
@@ -60,12 +70,17 @@ def on_left():
         currentchannel = 0
     else:
         currentchannel = currentchannel + 1
+
+    screen.text('Tuning to ' + channels[currentchannel])
         
     giphy_tv_url = get_channel(currentchannel)
     
     print giphy_tv_url
     
-    refresh_feed(1)
+    t = threading.Thread(target=refresh_feed, args=(1, ))
+    threads.append(t)
+    t.start()
+    print (len(threads))
 
 @every(seconds=5)
 def next_image():
@@ -92,7 +107,6 @@ def next_image():
     screen.update()    
 
 
-@every(minutes=10)
 def refresh_feed(user_refreshed = 0):
     screen.update()
     global imageloaded, state
@@ -137,10 +151,14 @@ def refresh_feed(user_refreshed = 0):
     state['image_urls'] = image_urls
     state['index'] = 0
     
+    return 1
+    
 def loop():
+    global threads
     if 'image' not in state:
         screen.fill(color='black')
         screen.text('Loading...')
+        refresh_feed()
         return
 
     image = state['image']
@@ -152,12 +170,11 @@ def loop():
     
     screen.image(state['image'], scale=sf)
     screen.update()
-
+    
 # run the app
 
 # Define first channel
 giphy_tv_url = get_channel(0)
-
 
 # Start loop
 tingbot.run(loop)
